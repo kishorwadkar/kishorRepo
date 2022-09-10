2bc5239d19b8439638ae6c0eb08b40a0448c7351
kishorwadkar <kishorwadkar1717@gmail.com>
2022-09-10 16:55:58 +0530

    change request register Patient compleated
    please give aproval to merge

--- OpenMRSApps/src/main/java/com/qa/pageLayer/RegisterPatient.java
+++ OpenMRSApps/src/main/java/com/qa/pageLayer/RegisterPatient.java
@@ -3,6 +3,7 @@ package com.qa.pageLayer;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import com.qa.TestBase.TestBase;
@@ -81,6 +82,142 @@ public class RegisterPatient extends TestBase
 	{
 		next_Button.click();
 	}
+	
+	@FindBy(xpath="//input[@id='birthdateDay-field']")    
+	public WebElement birthDate;
+	@Test
+	public void enterDateOfBirth(String date)
+	{
+		birthDate.sendKeys(date);
+	}
+	
+	@FindBy(xpath="//select[@id='birthdateMonth-field']")
+	public WebElement birthday_Month;
+	@Test
+	public void enterBirthDayMonth()
+	{
+		Select s=new Select(birthday_Month);
+		s.selectByIndex(5);
+		//birthday_Month.sendKeys(month);
+	}
+	
+	@FindBy(xpath="//input[@id='birthdateYear-field']")    
+	public WebElement birthDay_Year;
+	@Test
+	public void enterDateOfBirthYear(String year)
+	{
+		birthDay_Year.sendKeys(year);
+	}
+	
+	@FindBy(xpath="//icon[@class='fas fa-chevron-right']")
+	public WebElement next_Button3;
+	@Test
+	public void clickOnNextButton3()
+	{
+		next_Button3.click();
+	}
+	
+	
+	@FindBy(xpath="//input[@id='address1']")    
+	public WebElement Adress1;
+	@Test
+	public void enterAdressOfPatient(String adress)
+	{
+		Adress1.sendKeys(adress);
+	}
+	
+	
+	@FindBy(xpath="//input[@id='cityVillage']")    
+	public WebElement Villege_Name;
+	@Test
+	public void enterVillegeName(String villege)
+	{
+		Villege_Name.sendKeys(villege);
+	}
+	
+	@FindBy(xpath="//input[@id='stateProvince']")    
+	public WebElement State_Name;
+	@Test
+	public void enterStateName(String state)
+	{
+		State_Name.sendKeys(state);
+	}
+	
+	@FindBy(xpath="//input[@id='country']")    
+	public WebElement Country_Name;
+	@Test
+	public void enterCountryName(String country)
+	{
+		State_Name.sendKeys(country);
+	}
+	@FindBy(xpath="//input[@id='postalCode']")    
+	public WebElement Postal_code;
+	@Test
+	public void enterPostal(String postalCode)
+	{
+		Postal_code.sendKeys(postalCode);
+	}
+	
+
+	@FindBy(xpath="//button[@id='next-button']")
+	public WebElement next_Button4;
+	@Test
+	public void clickOnNextButton4()
+	{
+		next_Button4.click();
+	}
+	
+	@FindBy(xpath="//input[@name='phoneNumber']")
+	public WebElement phone_Number;
+	@Test
+	public void enterPhoneNumber(String Phone)
+	{
+		phone_Number.sendKeys(Phone);;
+	}
+	
+	@FindBy(xpath="//icon[@class='fas fa-chevron-right']")
+	public WebElement next_Button5;
+	@Test
+	public void clickOnNextButton5()
+	{
+		next_Button5.click();
+	}
+	
+	@FindBy(xpath="//select[@id='relationship_type']")
+	public WebElement RelationShip_Type;
+	@Test
+	public void SelectRelationShip_Type()
+	{
+		Select s=new Select(RelationShip_Type);
+		s.selectByIndex(5);
+		
+	}
+	
+	@FindBy(xpath="//input[@class='person-typeahead ng-pristine ng-untouched ng-valid ng-empty']")
+	public WebElement Relative_Name;
+	@Test
+	public void enterRelativeName(String Rname)
+	{
+		 Relative_Name.sendKeys(Rname);
+	}
+	@FindBy(xpath="//icon[@class='fas fa-chevron-right']")
+	public WebElement next_Button6;
+	@Test
+	public void clickOnNextButton6()
+	{
+		next_Button6.click();
+	}
+	
+	
+	@FindBy(xpath="//input[@id='submit']")
+	public WebElement submit_Button;
+	@Test
+	public void clickOnSubmitButton()
+	{
+		submit_Button.click();
+	}
+	
+	
 	
 
 }
--- OpenMRSApps/src/test/java/com/qa/testLayer/RegisterPatientTestCase.java
+++ OpenMRSApps/src/test/java/com/qa/testLayer/RegisterPatientTestCase.java
@@ -22,6 +22,28 @@ public class RegisterPatientTestCase extends TestBase
 	Thread.sleep(2000);
 	register.selectGenderAndClockOnIt();
 	register.clickOnNextButton();
+	Thread.sleep(2000);
+	register.enterDateOfBirth("20");
+	register.enterBirthDayMonth();
+	register.enterDateOfBirthYear("2018");
+	register.clickOnNextButton3();
+	Thread.sleep(2000);
+	register.enterAdressOfPatient("At:Wanewadi, Tal:Tuljapur,Dist:Osmanabad");
+	register.enterVillegeName("Wanewadi");
+	register.enterStateName("Maharashtra");
+	Thread.sleep(2000);
+	register.enterCountryName("India");
+	register.enterPostal("412634");
+	register.clickOnNextButton4();
+	Thread.sleep(2000);
+	
+	register.enterPhoneNumber("7020734961");
+	register.clickOnNextButton5();
+	Thread.sleep(2000);
+	register.SelectRelationShip_Type();
+	register.enterRelativeName("Pradip");
+	register.clickOnNextButton6();
+	register.clickOnSubmitButton();
 	
 	
 	}
